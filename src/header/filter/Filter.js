07d19eef1103d1475ce8074c22dd125f9f5825ac
helloelio <fredwantblossom@gmail.com>
2022-01-28 10:38:41 +0300

    Add Filter block

---
+++ src/header/filter/Filter.js
@@ -0,0 +1,12 @@
+import './Filter.css';
+
+function Filter() {
+  return (
+    <div className='filter'>
+      <span className='filter-title'>Filter block</span>
+      <button className='filter-btn'> # </button>
+    </div>
+  );
+}
+
+export default Filter;
